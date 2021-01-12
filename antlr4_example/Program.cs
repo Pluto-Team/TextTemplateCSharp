9bf1b443a120c6f70205941b2324874322ed55db
eisnerw <eisnerw@hotmail.com>
2021-01-12 15:33:38 -0500

    Added "test" sample template to program

--- antlr4_example/Program.cs
+++ antlr4_example/Program.cs
@@ -120,6 +120,525 @@ Subtemplates:
    !$1.#SameDay($0)->[{$1.ToDate().ToUpper()} at ]}{$1.ToDate('h:mma')
    }
 ]}";
+			input = @"{#tests.Assert(#expected,'ALL TESTS PASSED')}
+
+Subtemplates:
+{#test:[{$0.Matches(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)->
+[Test {$0} {$1.Assert($2,'passed')}]}]}
+{#test1:[
+Explicit Single Indent{#data:[
+   {lastName}]}]}
+{#test1Result:[
+Explicit Single Indent
+   Johnson
+   Smith]}
+{#test2:[
+Implied Single Indent {#data:[{lastName}]}]}
+{#test2Result:[
+Implied Single Indent 
+   Johnson
+   Smith]}
+{#test3:[
+Implied Double Indent{#data:[{lastName}{pets:[{name}]}]}]}
+{#test3Result:[
+Implied Double Indent
+   Johnson
+      Buddy
+      Ralph
+      Stripes
+   Smith
+      Toto
+      Dolly
+      Stripes]}
+{#test4:[
+Bulleted Implied Bulleted Indent{#data:[ //
+{.} {lastName}{pets:[{name}]}]}]}
+{#test4Result:[
+Bulleted Implied Bulleted Indent
+(0.0) Johnson
+   (1.0) Buddy
+   (1.1) Ralph
+   (1.2) Stripes
+(0.1) Smith
+   (1.0) Toto
+   (1.1) Dolly
+   (1.2) Stripes].Compose()}
+{#test5:[
+Bulleted Explicit Bulleted Indent{#data:[ //
+{.} {lastName}{pets:[ //
+    {.} {name}]}]}].Compose()}
+{#test5Result:[
+Bulleted Explicit Bulleted Indent
+(0.0) Johnson
+    (1.0) Buddy
+    (1.1) Ralph
+    (1.2) Stripes
+(0.1) Smith
+    (1.0) Toto
+    (1.1) Dolly
+    (1.2) Stripes]}
+{#test6:[
+Hierarchy{#data:[ //
+{.} {lastName}{pets:[ //
+    {.} {name} who is owned by 
+         {.} {^.^:[{firstName} who owns
+              {.} {pets:[{type}]}]}]}]}].Compose()}
+{#test6Result:[
+Hierarchy
+(0.0) Johnson
+    (1.0) Buddy who is owned by 
+         (2.0) Doris who owns
+              (3.0) dog
+              (3.1) lion
+              (3.2) tiger
+    (1.1) Ralph who is owned by 
+         (2.0) Doris who owns
+              (3.0) dog
+              (3.1) lion
+              (3.2) tiger
+    (1.2) Stripes who is owned by 
+         (2.0) Doris who owns
+              (3.0) dog
+              (3.1) lion
+              (3.2) tiger
+(0.1) Smith
+    (1.0) Toto who is owned by 
+         (2.0) John who owns
+              (3.0) dog
+              (3.1) cat
+              (3.2) zebra
+    (1.1) Dolly who is owned by 
+         (2.0) John who owns
+              (3.0) dog
+              (3.1) cat
+              (3.2) zebra
+    (1.2) Stripes who is owned by 
+         (2.0) John who owns
+              (3.0) dog
+              (3.1) cat
+              (3.2) zebra]}
+{#test7:[
+Hierarchy that transitions from indent to bullet {#data:[ //
+{lastName}{pets:[ //
+    {.} {name} who is owned by 
+         {.} {^.^:[{firstName} who owns
+            {.} {pets:[{type}]}]}]}]}]}
+{#test7Result:[
+Hierarchy that transitions from indent to bullet 
+Johnson
+    (0.0) Buddy who is owned by 
+         (1.0) Doris who owns
+            (2.0) dog
+            (2.1) lion
+            (2.2) tiger
+    (0.1) Ralph who is owned by 
+         (1.0) Doris who owns
+            (2.0) dog
+            (2.1) lion
+            (2.2) tiger
+    (0.2) Stripes who is owned by 
+         (1.0) Doris who owns
+            (2.0) dog
+            (2.1) lion
+            (2.2) tiger
+Smith
+    (0.0) Toto who is owned by 
+         (1.0) John who owns
+            (2.0) dog
+            (2.1) cat
+            (2.2) zebra
+    (0.1) Dolly who is owned by 
+         (1.0) John who owns
+            (2.0) dog
+            (2.1) cat
+            (2.2) zebra
+    (0.2) Stripes who is owned by 
+         (1.0) John who owns
+            (2.0) dog
+            (2.1) cat
+            (2.2) zebra]}
+{#test8:[
+Hierarchy that also transitions from indent to bullet {#data:[ //
+  {firstName} {lastName} who owns {pets:[{name} who is owned by 
+         {.} {^.^:[{firstName} who owns
+            {.} {pets:[{type}]}]}]}]}]}
+{#test8Result:[
+Hierarchy that also transitions from indent to bullet 
+  Doris Johnson who owns 
+     Buddy who is owned by 
+         (0.0) Doris who owns
+            (1.0) dog
+            (1.1) lion
+            (1.2) tiger
+     Ralph who is owned by 
+         (0.0) Doris who owns
+            (1.0) dog
+            (1.1) lion
+            (1.2) tiger
+     Stripes who is owned by 
+         (0.0) Doris who owns
+            (1.0) dog
+            (1.1) lion
+            (1.2) tiger
+  John Smith who owns 
+     Toto who is owned by 
+         (0.0) John who owns
+            (1.0) dog
+            (1.1) cat
+            (1.2) zebra
+     Dolly who is owned by 
+         (0.0) John who owns
+            (1.0) dog
+            (1.1) cat
+            (1.2) zebra
+     Stripes who is owned by 
+         (0.0) John who owns
+            (1.0) dog
+            (1.1) cat
+            (1.2) zebra]}
+{#test9:[
+Continued list {#data:[{pets:[{name}]}]}]}
+{#test9Result:[
+Continued list 
+   Buddy
+   Ralph
+   Stripes
+   Toto
+   Dolly
+   Stripes]}
+{#test10:[
+Bulleted Continued list ` 
+{#data:[{pets:[ 
+    {.} {name}]}]}]}
+{#test10Result:[
+Bulleted Continued list 
+    (0.0) Buddy
+    (0.1) Ralph
+    (0.2) Stripes
+    (0.3) Toto
+    (0.4) Dolly
+    (0.5) Stripes]}
+{#test11:[
+Hierarchy that also transitions from indent to bullet and back {#data:[ //
+{lastName} who owns 
+    {.} {pets:[{name} who is owned by 
+         {^.^:[{firstName} who owns 
+             {.} {pets:[{type} // 
+            ]}]}]}]}]}
+{#test11Result:[
+Hierarchy that also transitions from indent to bullet and back 
+Johnson who owns 
+    (0.0) Buddy who is owned by 
+         Doris who owns 
+             (1.0) dog
+             (1.1) lion
+             (1.2) tiger
+    (0.1) Ralph who is owned by 
+         Doris who owns 
+             (1.0) dog
+             (1.1) lion
+             (1.2) tiger
+    (0.2) Stripes who is owned by 
+         Doris who owns 
+             (1.0) dog
+             (1.1) lion
+             (1.2) tiger
+Smith who owns 
+    (0.0) Toto who is owned by 
+         John who owns 
+             (1.0) dog
+             (1.1) cat
+             (1.2) zebra
+    (0.1) Dolly who is owned by 
+         John who owns 
+             (1.0) dog
+             (1.1) cat
+             (1.2) zebra
+    (0.2) Stripes who is owned by 
+         John who owns 
+             (1.0) dog
+             (1.1) cat
+             (1.2) zebra]}
+{#test12:[
+Additional variation of hierarchy that also transitions from indent to bullet `
+ and back {#data:[ //
+{lastName} who owns 
+    {.} {pets:[{name} who is owned by 
+        {.} {^.^:[{firstName} who owns {pets:[{type} //  
+            ]}]}]}]}]}
+{#test12Result:[
+Additional variation of hierarchy that also transitions from indent to bullet and back 
+Johnson who owns 
+    (0.0) Buddy who is owned by 
+        (1.0) Doris who owns 
+           (2.0) dog
+           (2.1) lion
+           (2.2) tiger
+    (0.1) Ralph who is owned by 
+        (1.0) Doris who owns 
+           (2.0) dog
+           (2.1) lion
+           (2.2) tiger
+    (0.2) Stripes who is owned by 
+        (1.0) Doris who owns 
+           (2.0) dog
+           (2.1) lion
+           (2.2) tiger
+Smith who owns 
+    (0.0) Toto who is owned by 
+        (1.0) John who owns 
+           (2.0) dog
+           (2.1) cat
+           (2.2) zebra
+    (0.1) Dolly who is owned by 
+        (1.0) John who owns 
+           (2.0) dog
+           (2.1) cat
+           (2.2) zebra
+    (0.2) Stripes who is owned by 
+        (1.0) John who owns 
+           (2.0) dog
+           (2.1) cat
+           (2.2) zebra]}
+{#test13:[
+Yet another variation of hierarchy that also transitions from indent to bullet `
+ and back {#data:[ //
+{lastName} who owns 
+    {.} {pets:[{name} who is owned by 
+        {^.^:[{firstName} who owns {pets:[{type} //  
+            ]}]}]}]}]}
+{#test13Result:[
+Yet another variation of hierarchy that also transitions from indent to bullet and back 
+Johnson who owns 
+    (0.0) Buddy who is owned by 
+        Doris who owns 
+           dog
+           lion
+           tiger
+    (0.1) Ralph who is owned by 
+        Doris who owns 
+           dog
+           lion
+           tiger
+    (0.2) Stripes who is owned by 
+        Doris who owns 
+           dog
+           lion
+           tiger
+Smith who owns 
+    (0.0) Toto who is owned by 
+        John who owns 
+           dog
+           cat
+           zebra
+    (0.1) Dolly who is owned by 
+        John who owns 
+           dog
+           cat
+           zebra
+    (0.2) Stripes who is owned by 
+        John who owns 
+           dog
+           cat
+           zebra]}
+{#test14:[
+Indent under indent under bullet {#data:[ //
+{lastName} who owns 
+    {.} {pets:[{name} who is owned by 
+             {^.^:[{firstName} who owns 
+                 {pets:[{type} //  
+            ]}]}]}]}]}
+{#test14Result:[
+Indent under indent under bullet 
+Johnson who owns 
+    (0.0) Buddy who is owned by 
+             Doris who owns 
+                 dog
+                 lion
+                 tiger
+    (0.1) Ralph who is owned by 
+             Doris who owns 
+                 dog
+                 lion
+                 tiger
+    (0.2) Stripes who is owned by 
+             Doris who owns 
+                 dog
+                 lion
+                 tiger
+Smith who owns 
+    (0.0) Toto who is owned by 
+             John who owns 
+                 dog
+                 cat
+                 zebra
+    (0.1) Dolly who is owned by 
+             John who owns 
+                 dog
+                 cat
+                 zebra
+    (0.2) Stripes who is owned by 
+             John who owns 
+                 dog
+                 cat
+                 zebra]}
+{#test15:[
+Automatic bulleting for multi-values{#data:[ //
+{lastName} who owns 
+    {.} {pets:[{name} who is owned by 
+             {^.^:[{firstName} who owns 
+                 {pets:[{type} //  
+            ]}]}]}]}]}
+{#test15Result:[
+Automatic bulleting for multi-values
+Johnson who owns 
+    (0.0) Buddy who is owned by 
+             Doris who owns 
+                 dog
+                 lion
+                 tiger
+    (0.1) Ralph who is owned by 
+             Doris who owns 
+                 dog
+                 lion
+                 tiger
+    (0.2) Stripes who is owned by 
+             Doris who owns 
+                 dog
+                 lion
+                 tiger
+Smith who owns 
+    (0.0) Toto who is owned by 
+             John who owns 
+                 dog
+                 cat
+                 zebra
+    (0.1) Dolly who is owned by 
+             John who owns 
+                 dog
+                 cat
+                 zebra
+    (0.2) Stripes who is owned by 
+             John who owns 
+                 dog
+                 cat
+                 zebra]}
+{#test16:[
+{''.Compose() // reset numbering from previous tests
+}{[
+A:
+People:{'/data/people':[
+{.} Pets {[
+      {.} Domesticated{pets:[{type}]}
+   ]
+   } //
+].Compose()}
+B:
+People:{'/data/people':[
+   {.} {lastName}{pets:[{type}]}
+].@MissingValue('No Information Provided').Compose()}
+C:
+People:{'/data/people':[
+   {.} {lastName}{pets:[{type}]}
+].Compose()}   
+D:
+People:{'/data/people':[
+          {lastName}{pets:[{type}]}
+].@MissingValue('No Information Provided').Compose()}
+E:
+People:{'/data/people':[
+{.} {lastName}{pets:[
+ {.} {type}]}
+].@MissingValue('No Information Provided').Compose()}]}]}
+{#test16Result:[
+A:
+People:
+(0.0) Pets 
+      (1.0) Domesticated
+         (2.0) dog
+         (2.1) lion
+         (2.2) tiger
+(0.1) Pets 
+      (1.0) Domesticated
+         (2.0) dog
+         (2.1) cat
+         (2.2) zebra
+B:
+People:
+   (0.0) Johnson
+      (1.0) dog
+      (1.1) lion
+      (1.2) tiger
+   (0.1) Smith
+      (1.0) dog
+      (1.1) cat
+      (1.2) zebra
+C:
+People:
+   (0.0) Johnson
+      (1.0) dog
+      (1.1) lion
+      (1.2) tiger
+   (0.1) Smith
+      (1.0) dog
+      (1.1) cat
+      (1.2) zebra   
+D:
+People:Johnson
+   dog
+   lion
+   tiger
+Smith
+   dog
+   cat
+   zebra
+E:
+People:
+(0.0) Johnson
+ (1.0) dog
+ (1.1) lion
+ (1.2) tiger
+(0.1) Smith
+ (1.0) dog
+ (1.1) cat
+ (1.2) zebra]}
+{#tests:[ //
+{'1'.#test(#test1,#test1Result)}
+{'2'.#test(#test2,#test2Result)}
+{'3'.#test(#test3,#test3Result)}
+{'4'.#test(#test4,#test4Result)}
+{'5'.#test(#test5,#test5Result)}
+{'6'.#test(#test6,#test6Result)}
+{'7'.#test(#test7,#test7Result)}
+{'8'.#test(#test8,#test8Result)}
+{'9'.#test(#test9,#test9Result)}
+{'10'.#test(#test10,#test10Result)}
+{'11'.#test(#test11,#test11Result)}
+{'12'.#test(#test12,#test12Result)}
+{'13'.#test(#test13,#test13Result)}
+{'14'.#test(#test14,#test14Result)}
+{'15'.#test(#test15,#test15Result)}
+{'16'.#test(#test16,#test16Result)}
+]}
+{#expected:[ //
+Test 1 passed
+Test 2 passed
+Test 3 passed
+Test 4 passed
+Test 5 passed
+Test 6 passed
+Test 7 passed
+Test 8 passed
+Test 9 passed
+Test 10 passed
+Test 11 passed
+Test 12 passed
+Test 13 passed
+Test 14 passed
+Test 15 passed
+Test 16 passed
+]}
+{#data:[/data/people]}";
 			input = input.Replace("\r", "");
 			AntlrInputStream inputStream = new AntlrInputStream(input);
 			TextTemplateLexer textTemplateLexer = new TextTemplateLexer(inputStream);
